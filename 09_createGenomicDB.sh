1ea6be1272d2a5a1f9db9c2cb7d5fa4aa7fe6712
Dr. Rhiannon Peery <rmpeery@gmail.com>
2025-12-18 09:35:18 -0800

    Create 'beds' directory and rename interval files
    Organize output files into 'beds' directory and rename them.

--- 09_createGenomicDB.sh
+++ 09_createGenomicDB.sh
@@ -11,6 +11,10 @@ awk '{print $1, "0", $2}' <infile.fai >outfile.bed
 
 ## 3. split into intervals
 split -l 500 interval_batch outfile.bed
+mkdir beds
+mv interval_batch* beds
+cd beds
+for FILE in *; do mv ${FILE} ${FILE}.bed; done
 
 ## 4. gatk dict file
 module load StdEnv/2023 gatk/4.4.0.0
